dc57b6039fc244617ff818e44282c196930ef4f7
Muhammad Hafizh Abdillah AR <hafizh.abdillahh@gmail.com>
2021-06-17 23:53:19 +0700

    build compiler-cli webpack-for-development-hmr

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/server/response/http-exception/http_exception.filter';
 import { AppConfigService } from './config/app/config.service';
-
+declare const module: any;
 async function boostrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig: AppConfigService = app.get('AppConfigService');
@@ -11,5 +11,9 @@ async function boostrap() {
   await app.listen(appConfig.port, () => {
       console.log('Application Running On Port: ', appConfig.port)
   });
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 boostrap();
