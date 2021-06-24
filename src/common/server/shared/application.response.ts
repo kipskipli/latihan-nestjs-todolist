0079e3640730b3698c375665e440c7858c43bcae
Muhammad Hafizh Abdillah AR <hafizh.abdillahh@gmail.com>
2021-06-24 16:11:34 +0700

    fix missing Response

--- src/common/server/shared/application.response.ts
+++ src/common/server/shared/application.response.ts
@@ -1,15 +1,21 @@
 import { HttpStatus, Res } from "@nestjs/common";
 import { AccessTokenInterface } from "src/common/interface";
 import { HttpResponseDto } from "../response/dto/http_response.dto";
-
+import { Response } from "express";
 export class ApplicationResponse {
-  static ok(@Res() res, data: any, token?: AccessTokenInterface): Response {
+  static ok(
+    @Res() res: Response,
+    data: any,
+    token?: AccessTokenInterface,
+  ): Response {
     return res
       .status(HttpStatus.OK)
       .json(new HttpResponseDto(true, "Success", data, token));
   }
-
-  static accepted(@Res() res, token?: AccessTokenInterface): Response {
+  static accepted(
+    @Res() res: Response,
+    token?: AccessTokenInterface,
+  ): Response {
     return res
       .status(HttpStatus.ACCEPTED)
       .json(new HttpResponseDto(true, "Accepted", null, token));
