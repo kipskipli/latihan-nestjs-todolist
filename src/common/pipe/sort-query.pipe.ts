84a3c83251ade9aa7426836d48a050a0cda0d598
adilpratama71 <adilpratama71@yahoo.com>
2021-09-08 19:48:54 +0700

    update sort validation pipe

--- src/common/pipe/sort-query.pipe.ts
+++ src/common/pipe/sort-query.pipe.ts
@@ -4,15 +4,31 @@ import { ISortQuery } from "@inspigoid/inspigo-utils-ts/lib/interface";
 
 @Injectable()
 export class SortQueryValidationPipe implements PipeTransform<unknown> {
-  constructor(private readonly sortObj: Record<string, ISortQuery>) {}
+  constructor(private readonly sortArr: string[]) {}
   transform(value: string): ISortQuery {
     if (value && typeof value !== "string") {
       throw new BadRequestException({ sort: ["Invalid Sort Option"] });
     }
-    const sortOption = this.sortObj[value];
-    if (!sortOption) {
+    const sortQuery = this.checkAscOrDesc(value, this.sortArr);
+    if (!sortQuery) {
       return { u_at: -1 };
     }
-    return sortOption;
+    return sortQuery;
+  }
+
+  private checkAscOrDesc(value: string, sortArr: string[]) {
+    let prefix = "";
+    let sortValue = 0;
+    if (value.endsWith("_asc")) {
+      prefix = "_asc";
+      sortValue = 1;
+    } else if (value.endsWith("_desc")) {
+      prefix = "_desc";
+      sortValue = -1;
+    } else {
+      return null;
+    }
+    const [field, _] = value.split(prefix);
+    return sortArr.indexOf(field) >= 0 ? { [field]: sortValue } : null;
   }
 }
